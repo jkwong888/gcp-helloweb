368a7d63fcdb75172cd02ca64f36f36d2cca0982
jkwng <jkwng@google.com>
2022-01-06 12:02:07 -0500

    add gce attributers

--- main.go
+++ main.go
@@ -27,6 +27,7 @@ import (
 	"os"
 	"strings"
 	"time"
+	"regexp"
 
 	"contrib.go.opencensus.io/exporter/prometheus"
 	"github.com/heptiolabs/healthcheck"
@@ -34,6 +35,52 @@ import (
 	"go.opencensus.io/plugin/ochttp"
 	"go.opencensus.io/stats/view"
 )
+
+const PAYLOAD_VERSION = "1.1.0"
+
+type payload struct {
+	version string
+
+	nodeName string
+	zone string
+	project string
+	serviceAccount string
+
+	guest guestAttrs
+	client clientAttrs
+
+	gce *gceAttrs
+	gke *gkeAttrs
+	run *runAttrs
+	cf *cfAttrs
+}
+
+type guestAttrs struct {
+	hostname string
+}
+
+type clientAttrs struct {
+	sourceIpAddr string
+	lbIpAddr *string
+}
+
+type gceAttrs struct {
+	privateIpAddr string
+	machineType string
+	preemptible bool
+	migName *string
+}
+
+type gkeAttrs struct {
+	clusterName string
+	clusterRegion string
+}
+
+type runAttrs struct {
+}
+
+type cfAttrs struct {
+}
 
 func getMetaData(ctx context.Context, path string) *string {
 	metaDataURL := "http://metadata/computeMetadata/v1/"
@@ -105,7 +152,6 @@ func NullHealthCheck() healthcheck.Check {
 }
 
 func enableHealthCheck(mux *http.ServeMux) {
-
 	// add health check
 	health := healthcheck.NewHandler()
 
@@ -140,6 +186,137 @@ func enableHealthCheck(mux *http.ServeMux) {
 	*/
 }
 
+func getAllAttrs() payload {
+	allVals := payload{}
+	ctx := context.Background()
+
+	allVals.version = PAYLOAD_VERSION
+
+	nodeName := getMetaData(ctx, "instance/hostname")
+	if nodeName != nil {
+		allVals.nodeName = *nodeName
+	}
+
+	zoneStr := getMetaData(ctx, "instance/zone")
+	if zoneStr != nil {
+		zoneArr := strings.Split(*zoneStr, "/")
+		zone := zoneArr[len(zoneArr)-1]
+
+		allVals.zone = zone
+	}
+
+	project := getMetaData(ctx, "project/project-id")
+	if project != nil {
+		allVals.project = *project
+	}
+
+	serviceAccount := getMetaData(ctx, "instance/service-accounts/default/email")
+	if serviceAccount != nil {
+		allVals.serviceAccount = *serviceAccount
+	}
+
+	/* Begin guest attributes */
+	host, _ := os.Hostname()
+	allVals.guest.hostname = host
+	/* End guest attributes */
+
+	/* Begin GCE attributes */
+	machineType := getMetaData(ctx, "instance/machine-type")
+	if machineType != nil {
+		// assumption: all GCE machines will have the machine-type property
+		if allVals.gce == nil {
+			allVals.gce = &gceAttrs{}
+		}
+
+		rexp := regexp.MustCompile(`.*/machineTypes/`)
+		machineTypeStr := rexp.ReplaceAllString(*machineType, "")
+		allVals.gce.machineType = machineTypeStr
+	}
+	
+	internalIP := getMetaData(ctx, "instance/network-interfaces/0/ip")
+	if internalIP != nil {
+		allVals.gce.privateIpAddr = *internalIP
+	}
+
+	createdBy := getMetaData(ctx, "instance/attributes/created-by")
+	if createdBy != nil {
+		rexp := regexp.MustCompile(`.*/instanceGroupManagers/`)
+		migNameStr := rexp.ReplaceAllString(*createdBy, "")
+		allVals.gce.migName = &migNameStr
+	}
+
+	preemptible := getMetaData(ctx, "instance/scheduling/preemptible")
+	if preemptible != nil  && *preemptible == "TRUE" {
+		allVals.gce.preemptible = true
+	} else {
+		allVals.gce.preemptible = false
+	}
+
+	/* End GCE attributes */
+
+	/* Begin GKE attributes */
+	clusterName := getMetaData(ctx, "instance/attributes/cluster-name")
+	if clusterName != nil {
+		if allVals.gke == nil {
+			allVals.gke = &gkeAttrs{}
+		}
+
+		allVals.gke.clusterName = *clusterName
+	}
+
+	region := getMetaData(ctx, "instance/attributes/cluster-location")
+	if region != nil {
+		if allVals.gke == nil {
+			allVals.gke = &gkeAttrs{}
+		}
+
+		allVals.gke.clusterName = *clusterName
+	}
+
+	/* End GKE attributes */
+
+	return allVals
+}
+
+
+
+// hello responds to the request with a plain-text "Hello, world" message.
+func hello(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Serving request: %s", r.URL.Path)
+
+	attrs := getAllAttrs()
+
+	fmt.Fprintf(w, "Hello, world!\n")
+	fmt.Fprintf(w, "Version: %s\n", attrs.version)
+	fmt.Fprintf(w, "Hostname: %s\n", attrs.guest.hostname)
+
+	fmt.Fprintf(w, "Zone: %s\n", attrs.zone)
+	fmt.Fprintf(w, "Project: %s\n", attrs.project)
+	fmt.Fprintf(w, "Node FQDN: %s\n", attrs.nodeName)
+	fmt.Fprintf(w, "Service Account: %s\n", attrs.serviceAccount)
+
+	//	fmt.Fprintf(w, "Internal IP: %s\n", internalIP)
+
+	// if we're in a VM
+	if attrs.gce != nil {
+		fmt.Fprintf(w, "Machine Type: %s\n", attrs.gce.machineType)
+		fmt.Fprintf(w, "Internal IP Address: %s\n", attrs.gce.privateIpAddr)
+		fmt.Fprintf(w, "Preemptible: %t\n", attrs.gce.preemptible)
+
+		if attrs.gce.migName != nil {
+			fmt.Fprintf(w, "Managed Instance Group: %s\n", *attrs.gce.migName)
+		}
+	}
+
+	// if we're in GKE 
+	if attrs.gke != nil {
+		// get the cluster name
+		fmt.Fprintf(w, "GKE Cluster Name: %s\n", attrs.gke.clusterName)
+		fmt.Fprintf(w, "GKE Cluster Region: %s\n", attrs.gke.clusterRegion)
+	}
+
+}
+
 func main() {
 
 	// Firstly, we'll register ochttp Server views.
@@ -170,46 +347,4 @@ func main() {
 	err := http.ListenAndServe(":"+port, h)
 	log.Fatal(err)
 }
-
-// hello responds to the request with a plain-text "Hello, world" message.
-func hello(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Serving request: %s", r.URL.Path)
-	ctx := context.Background()
-
-	host, _ := os.Hostname()
-	zoneStr := getMetaData(ctx, "instance/zone")
-	nodeName := getMetaData(ctx, "instance/hostname")
-	region := getMetaData(ctx, "instance/attributes/cluster-location")
-	clusterName := getMetaData(ctx, "instance/attributes/cluster-name")
-	project := getMetaData(ctx, "project/project-id")
-	//internalIP := getMetaData(ctx, "instance/network-interfaces/0/ip")
-
-	fmt.Fprintf(w, "Hello, world!\n")
-	fmt.Fprintf(w, "Version: 1.0.0\n")
-	fmt.Fprintf(w, "Hostname: %s\n", host)
-	if nodeName != nil {
-		fmt.Fprintf(w, "Node Name: %s\n", *nodeName)
-	}
-
-	if clusterName != nil {
-		fmt.Fprintf(w, "Cluster Name: %s\n", *clusterName)
-	}
-
-	if region != nil {
-		fmt.Fprintf(w, "Cluster Region: %s\n", *region)
-	}
-
-	if zoneStr != nil {
-		zoneArr := strings.Split(*zoneStr, "/")
-		zone := zoneArr[len(zoneArr)-1]
-
-		fmt.Fprintf(w, "Zone: %s\n", zone)
-	}
-	if project != nil {
-		fmt.Fprintf(w, "Project: %s\n", *project)
-	}
-	//	fmt.Fprintf(w, "Internal IP: %s\n", internalIP)
-
-}
-
 // [END all]
